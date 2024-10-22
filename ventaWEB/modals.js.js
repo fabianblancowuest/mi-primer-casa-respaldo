4d61a99ff64b04b32bed220fb319b4ceacaa25f6
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-10-22 19:35:50 -0300

    Actualizar modales

--- ventaWEB/modals.js.js
+++ ventaWEB/modals.js.js
@@ -1,4 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
+	const modalPremios = document.getElementById("premiosPrincipales");
+	const botonPremios = document.getElementById("obutPremios");
+	const closePremios = document.getElementById("closeModalPremios");
+
+	modalPremios.style.display = "none";
+
+	botonPremios.addEventListener("click", (event) => {
+		event.preventDefault();
+		modalPremios.style.display = "flex";
+
+		document.body.classList.add("no-scroll"); // Bloquea el scroll del fondo
+	});
+
+	closeModalPremios.addEventListener("click", () => {
+		modalPremios.style.display = "none";
+		console.log("Cerrando");
+		document.body.classList.remove("no-scroll"); // Restablece el scroll del fondo
+	});
+
 	const btnRealizarPago = document.getElementById("obutRealizarPago");
 
 	const modal = document.getElementById("myModal");
@@ -32,16 +51,16 @@ document.addEventListener("DOMContentLoaded", () => {
 			modal.style.display = "none";
 			document.body.classList.remove("no-scroll"); // Restablece el scroll del fondo
 		}
+
+		if (event.target === modalPremios) {
+			modalPremios.style.display = "none";
+			console.log("hola");
+			document.body.classList.remove("no-scroll"); // Restablece el scroll del fondo
+		}
 	});
 
 	//Simula un clic automático en el botón openModalBtn una vez al cargar la página
 	setTimeout(() => {
 		openModalBtn.click();
-	}, 1000);
-
-	const btnPremiosPrincipales = document.getElementById("obutPremios");
-
-	btnPremiosPrincipales.click(() => {
-		alert("Click");
-	});
+	}, 500);
 });
